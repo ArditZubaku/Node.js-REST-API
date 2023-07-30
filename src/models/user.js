59d548d3ce7369df9a3e050aab45a8e1a537f187
ArditZubaku <zubaku92@gmail.com>
2023-07-30 06:19:53 +0200

    Added some user methods.

--- src/models/user.js
+++ src/models/user.js
@@ -10,6 +10,33 @@ class User {
     this.createdAt = new Date();
     this.updatedAt = new Date();
   }
+
+  static collectionName() {
+    return "users";
+  }
+
+  static async connect() {
+    const db = await connectToDatabase();
+    this.collection = db.collection(this.collectionName());
+  }
+
+  static async findByUsername(username) {
+    return this.collection.findOne({ username });
+  }
+
+  static async findById(id) {
+    return this.collection.findOne({ _id: ObjectId(id) });
+  }
+
+  static async create(user) {
+    const salt = await bcrypt.genSalt();
+    user.password = await bcrypt.hash(user.password, salt);
+    await this.collection.insertOne(user);
+  }
+
+  static async comparePasswords(passswordToCompare, hashedPassword) {
+    return await bcrypt.compare(passswordToCompare, hashedPassword);
+  }
 }
 
 module.exports = User;
