614b35fcfc00607e0d0e4781890587bbce2d57b9
ArditZubaku <zubaku92@gmail.com>
2023-07-30 20:51:16 +0200

    Updated generateToken function.

--- src/utils/generateToken.js
+++ src/utils/generateToken.js
@@ -1,8 +1,11 @@
 const jwt = require("jsonwebtoken");
-const { JWT_SECRET } = process.env;
+const {JWT_SECRET} = process.env;
 
-function generateToken(user) {
-  return jwt.sign(user, JWT_SECRET, { expiresIn: "1h" });
+const generateToken = (user) => {
+    const {id, full_name: fullName, email, username} = user
+    return jwt.sign({
+        id, fullName, email, username
+    }, JWT_SECRET, {expiresIn: "1h"});
 }
 
 module.exports = generateToken;
